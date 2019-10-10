2ccca70ada0073e51472a1485ca02336e61284aa
Vrushali Vaidya <vvaidya@tiuconsulting.com>
2019-10-10 11:56:10 +0530

    updated code

--- src/test/java/com/org/MyMaven/HellowWorldTest.java
+++ src/test/java/com/org/MyMaven/HellowWorldTest.java
@@ -1,8 +1,13 @@
 package com.org.MyMaven;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 import java.awt.Desktop.Action;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,15 +16,15 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+@Test
 public class HellowWorldTest {
 	private WebDriver driver;
 	private String baseUrl;
+	public String Email;
+	public String Password;
 	
 	@BeforeClass(alwaysRun = true)
 	  public void setUp() throws Exception {
-		  //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\lib\\chromedriver.exe");
-		  //WebDriver driver= new ChromeDriver();
-		  //System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\lib\\geckodriver.exe");
 		  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\lib\\chromedriver.exe");
 		  driver= new ChromeDriver();
 		 // baseUrl = "http://packagingsuppliesbymail.com/staging";
@@ -27,49 +32,50 @@ public class HellowWorldTest {
 		  //driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	  }
 	  
-	@Test (enabled=true)
 	public void Test() throws InterruptedException
 	{ 
-		/*System.out.println("Hello World");
-		System.out.println(System.getProperty("user.dir"));*/
-		
-		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\Vrushali Vaidya\\eclipse-workspace\\MyMavenTest\\lib\\geckodriver.exe");
-		
 		driver.navigate().to(baseUrl);
 		driver.manage().window().maximize();
-		//driver.manage().window().setSize(new Dimension(1536, 824));
-	    driver.findElement(By.linkText("Login")).click();
-	    driver.findElement(By.id("email")).click();
-	    driver.findElement(By.id("email")).sendKeys("vvaidya@tiuconsulting.com");
-	    driver.findElement(By.id("pass")).sendKeys("tiu@12345");
-	    //driver.findElement(By.cssSelector(".primary:nth-child(1) > #send2 > span")).click();
-	  //  driver.findElement(By.linkText("Sign Out")).click();
-	    driver.findElement(By.xpath("//fieldset[@class='fieldset login']//span[contains(text(),'Sign In')]")).click();
-	   /* {
-	      WebElement element = driver.findElement(By.linkText("Sign Out"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }
+		driver.findElement(By.linkText("Login")).click();
+		
+		//Array for multiple login inputs
+		String arr[][]=  {{"vvaidya","vvaidya@tiuconsulting.com", "vvaidya@tiuconsulting.com"}};
+		String app[][]= {{"123456","Test@123", "tiu@12345"}};
+		for (int i=0; i<arr.length; i++)
+		{
+			driver.findElement(By.id("email")).click();
+			driver.findElement(By.xpath("//input[@title='Email']")).clear();
+			driver.findElement(By.xpath("//input[@title='Email']")).sendKeys(arr[i]);	
+			driver.findElement(By.id("pass")).click(); 
+			driver.findElement(By.id("pass")).clear();
+		    driver.findElement(By.id("pass")).sendKeys(app[i]);
+		    driver.findElement(By.cssSelector(".primary:nth-child(1) > #send2 > span")).click();
+		    Thread.sleep(2000);
+		}
+	
+		
+
+	
+		
+		//driver.findElement(By.xpath(".//*[html/body/div[1]/header/div[1]/div/div/div[2]/div/ul[2]/li[2]/a]")).click();
+		// /html/body/div[1]/header/div[1]/div/div/div[2]/div/ul[2]/li[2]/a
+	 
+		/*driver.findElement(By.id("email")).click();
+
+	    driver.findElement(By.id("pass")).sendKeys("tiu123");
+	   if 
 	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element, 0, 0).perform();
-	    }
+	        WebElement element = driver.findElement(By.cssSelector(".primary:nth-child(1) > #send2 > span"));
+	        Actions builder = new Actions(driver);
+	        builder.moveToElement(element).perform();
+	      }
 	    {
-	      WebElement element = driver.findElement(By.linkText("Sign Out"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element, 0, 0).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.linkText("Sign Out"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }*/
+	        WebElement element = driver.findElement(By.tagName("body"));
+	        Actions builder = new Actions(driver);
+	        builder.moveToElement(element, 0, 0).perform();
+	      }
+	   	    */
+	   
 		//Thread.sleep(2000);
 		//driver.close();
 		//driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='REVIEWS'])[1]/following::img[4]")).click();
@@ -80,5 +86,5 @@ public class HellowWorldTest {
 	    //driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Stretch Wrap'])[3]/following::img[1]")).click();
 	    //driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Cast Stretch Wrap'])[2]/following::img[1]")).click();
 	}
-
 }
+
